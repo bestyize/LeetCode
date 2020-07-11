3af1f011ea85db623ddaaefd81576dcf8cd4c6e7
yize <3011429848@qq.com>
2020-07-12 00:11:40 +0800

    case:min cover str bug fix

--- ByteDance/src/com/yize/leetcode/MinCoverString.java
+++ ByteDance/src/com/yize/leetcode/MinCoverString.java
@@ -1,5 +1,10 @@
 package com.yize.leetcode;
 
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
  *
@@ -17,13 +22,69 @@ package com.yize.leetcode;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class MinCoverString {
+    @Test
+    public void test(){
+        System.out.println(minWindow("aa","aa"));
+    }
+    public String minWindow1(String s, String t) {
+        Map<Character, Integer> map = new HashMap();
+        Map<Character, Integer> tempMap = new HashMap();
+        for (int i = t.length() - 1; i >= 0; i--) {
+            if (map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i), map.get(t.charAt(i)) + 1);
+            } else {
+                map.put(t.charAt(i), 1);
+            }
+        }
+
+        int left=0,right=0;
+        int matchCount=0;
+        int start=0;
+        int minLen=Integer.MAX_VALUE;
+        while (right<s.length()){
+            char c=s.charAt(right);
+            if(map.containsKey(c)){
+                if(tempMap.containsKey(c)){
+                    tempMap.put(c,tempMap.get(c)+1);
+                }else {
+                    tempMap.put(c,1);
+                }
+                if(map.get(c)==(int)tempMap.get(c)){
+                    matchCount++;
+                }
+            }
+            right++;
+            while (matchCount==map.size()){
+                if(right-left<minLen){
+                    start=left;
+                    minLen=right-left;
+                    //add
+                }
+                char cc=s.charAt(left);
+                if(map.containsKey(cc)){
+                    tempMap.put(cc,tempMap.get(cc)-1);
+                    if(tempMap.get(cc)<(int)map.get(cc)){
+                        matchCount--;
+                    }
+                }
+                left++;
+            }
+
+        }
+        return minLen==Integer.MAX_VALUE?"":s.substring(start,start+minLen);
+    }
+
     public String minWindow(String s, String t) {
         int[] map=new int[128];
         int[] tempMap=new int[128];
         char[] cs=s.toCharArray();
         char[] ts=t.toCharArray();
+        int noDupCount=0;
         for (char c:ts){
             map[c]++;
+            if(map[c]==1){
+                noDupCount++;
+            }
         }
         int left=0,right=0;
         int matchCount=0;
@@ -38,7 +99,7 @@ public class MinCoverString {
                 }
             }
             right++;
-            while (matchCount==ts.length){
+            while (matchCount==noDupCount){
                 if(right-left<minLen){
                     start=left;
                     minLen=right-left;
