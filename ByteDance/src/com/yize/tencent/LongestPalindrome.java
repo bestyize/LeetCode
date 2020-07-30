527d69ee24ff41db5db9b8ed466be58b4edc2803
bestyize <3011429848@qq.com>
2020-07-30 09:54:18 +0800

    case: cases of leetcode

---
+++ ByteDance/src/com/yize/tencent/LongestPalindrome.java
@@ -0,0 +1,25 @@
+package com.yize.tencent;
+
+public class LongestPalindrome {
+    public String longestPalindrome(String str){
+        int maxLen=0,maxStart=0;
+        char cs[]=str.toCharArray();
+        boolean[][] dp=new boolean[cs.length][cs.length];
+        for (int i=0;i<cs.length;i++){
+            for (int j=i;j>=0;j--){
+                if(i-j<=2){
+                    dp[j][i]=cs[i]==cs[j];
+                }else {
+                    dp[j][i]=(cs[i]==cs[j])&&dp[j+1][i-1];
+                }
+                if(dp[j][i]==true){
+                    if(j-i>maxLen){
+                        maxLen=j-i;
+                        maxStart=j;
+                    }
+                }
+            }
+        }
+        return String.copyValueOf(cs,maxStart,maxLen);
+    }
+}
